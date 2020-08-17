cf717b1e6024fa11c208bedfe288e65a80a7b055
Adrián Arcalá <aarcala10@gmail.com>
2020-08-18 00:54:55 +0200

    Pintamos lista de países en página Inicio

--- src/components/pages/home/view.js
+++ src/components/pages/home/view.js
@@ -2,22 +2,34 @@ import React from 'react';
 import {View, Text} from 'react-native';
 import styles from './styles';
 import {getCountries} from '../../../api';
+import {} from 'react-native-reanimated';
 
 class Home extends React.Component {
-  componentDidMount() {
-    getCountries()
-      .then((res) => {
-        console.log('countries:', res);
-      })
-      .catch((e) => {
-        console.log('error:', e);
-      });
+  constructor(props) {
+    super(props);
+    this.state = {list: []};
+  }
+  async componentDidMount() {
+    try {
+      const getCountriesRes = await getCountries();
+      console.log('getCountries:', getCountriesRes);
+      const list = getCountriesRes.data.countries;
+      this.setState({list});
+      //this.setState({list: list});
+    } catch (e) {
+      console.log('getCountries error: ', e);
+    }
   }
 
   render() {
+    console.log('List countries: ', this.state.list);
     return (
       <View style={styles.container}>
-        <Text>HOME</Text>
+        {this.state.list.map((v, i) => (
+          <Text key={`cell-${i}`} style={{marginVertical: 20, color: 'grey'}}>
+            {v.name}
+          </Text>
+        ))}
       </View>
     );
   }
