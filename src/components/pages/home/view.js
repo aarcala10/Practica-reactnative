d6c9b9b1ab69b3b96b4b21a265a9831fa771331e
Adrián Arcalá <aarcala10@gmail.com>
2020-08-19 01:46:48 +0200

    Flatlist en la página de inicio

--- src/components/pages/home/view.js
+++ src/components/pages/home/view.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Image, Alert, FlatList, Dimensions} from 'react-native';
 import styles from './styles';
 import {getCountries} from '../../../api';
 import {} from 'react-native-reanimated';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+
+const {width, height} = Dimensions.get('window');
 
 class Home extends React.Component {
   constructor(props) {
@@ -17,19 +20,34 @@ class Home extends React.Component {
       this.setState({list});
       //this.setState({list: list});
     } catch (e) {
-      console.log('getCountries error: ', e);
+      Alert.alert('Alert', 'Ha ocurrido un error');
     }
   }
+
+  _renderItem = ({item}) => {
+    return (
+      <TouchableOpacity
+        onPress={() => Alert.alert('Atención', `${item.name} pulsado`)}
+        style={{width: width / 2}}>
+        <Image
+          resizeMode={'contain'}
+          source={{uri: item.flag}}
+          style={{width: '100%', height: 150}}
+        />
+      </TouchableOpacity>
+    );
+  };
 
   render() {
     console.log('List countries: ', this.state.list);
     return (
       <View style={styles.container}>
-        {this.state.list.map((v, i) => (
-          <Text key={`cell-${i}`} style={{marginVertical: 20, color: 'grey'}}>
-            {v.name}
-          </Text>
-        ))}
+        <FlatList
+          data={this.state.list}
+          keyExtractor={(item, index) => `card-${item.code}`}
+          numColumns={2}
+          renderItem={this._renderItem}
+        />
       </View>
     );
   }
